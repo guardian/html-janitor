e3d77b699ba51b900f4d37487ff838670a23504d
Oliver Joseph Ash <oliverjash@icloud.com>
2013-10-14 18:03:43 +0100

    Submit failing test for cleaning multiple attributes

--- test/index.js
+++ test/index.js
@@ -6,4 +6,9 @@ describe('janitor', function() {
     var result = janitor.clean('<b>uh oh!', {tags: {b: []}});
     expect(result).to.eql('<b>uh oh!</b>');
   });
+
+  it('cleans elements with more than one attribute', function () {
+    var result = janitor.clean('<p class="uh oh!" style="font-size: 24px;"></p>', {tags: {p: []}});
+    expect(result).to.eql('<p></p>');
+  });
 });
