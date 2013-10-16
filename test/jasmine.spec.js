2abcb71194f4dc08c6f51613d6169f2e863cc581
Oliver Joseph Ash <oliverjash@icloud.com>
2013-10-16 11:31:40 +0100

    Setup Karma with RequireJS

---
+++ test/jasmine.spec.js
@@ -0,0 +1,7 @@
+define([ 'janitor' ], function (janitor) {
+  describe('janitor', function () {
+    it('should have foo property', function () {
+      expect(janitor.foo).toBe('bar');
+    });
+  });
+});
