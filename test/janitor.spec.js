d46527f2c62adf8fab593adf1ad2b912f202df26
Oliver Joseph Ash <oliverjash@icloud.com>
2013-10-16 12:26:15 +0100

    Use existing logic

--- test/janitor.spec.js
+++ test/janitor.spec.js
@@ -1,7 +1,22 @@
-define([ 'janitor' ], function (janitor) {
+define([ 'janitor' ], function (Janitor) {
+
   describe('janitor', function () {
-    it('should have foo property', function () {
-      expect(janitor.foo).toBe('bar');
+    var janitor;
+    var config = {
+      tags: {
+        p: []
+      }
+    };
+
+    beforeEach(function () {
+      janitor = new Janitor(config);
+    });
+
+    it('should clean attributes not in the whitelist', function () {
+      var p = document.createElement('p');
+      p.setAttribute('style', 'font-size: 16px;');
+      expect(janitor.clean(p.outerHTML)).toBe('<p></p>');
     });
   });
+
 });
