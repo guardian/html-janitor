cb3603d81306122f07b0d9e3730fa2e76be38ee8
Oliver Joseph Ash <oliverjash@icloud.com>
2013-10-16 12:25:48 +0100

    Correct indentation

--- src/janitor.js
+++ src/janitor.js
@@ -1,12 +1,12 @@
 // UMD
 (function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        define(factory);
-    } else {
-        root.amdWeb = factory();
-    }
+  if (typeof define === 'function' && define.amd) {
+    define(factory);
+  } else {
+    root.amdWeb = factory();
+  }
 }(this, function () {
-    return {
-      foo: 'bar'
-    };
+  return {
+    foo: 'bar'
+  };
 }));
