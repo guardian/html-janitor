5251c796d5257a9a3af779ba80d5ff39ae4251c9
Oliver Joseph Ash <oliverjash@icloud.com>
2013-10-16 12:28:42 +0100

    Test elements

--- test/janitor.spec.js
+++ test/janitor.spec.js
@@ -17,6 +17,13 @@ define([ 'janitor' ], function (Janitor) {
       p.setAttribute('style', 'font-size: 16px;');
       expect(janitor.clean(p.outerHTML)).toBe('<p></p>');
     });
+
+    it('should remove elements not in the whitelist', function () {
+      var div = document.createElement('div');
+      var p = document.createElement('p');
+      div.appendChild(p);
+      expect(janitor.clean(div.outerHTML)).toBe('<p></p>');
+    });
   });
 
 });
